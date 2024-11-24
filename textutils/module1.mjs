fed2d37a0740a9445d5c86a2c4382646724326fb
tusharx0809 <tusharbhatia0809@outlook.com>
2024-11-24 16:38:02 +0530

    sending-app

---
+++ textutils/module1.mjs
@@ -0,0 +1,9 @@
+const a = "Tushar";
+const b = "Vimal";
+const c = "abcde";
+const d = "fghij"
+
+export default b;
+export {a};
+export {c};
+export {d};
---
+++ textutils/module2.mjs
@@ -0,0 +1,5 @@
+import b_vimal, {a, c, d} from './module1.mjs'
+console.log(b_vimal);
+console.log(a);
+console.log(c);
+console.log(d);
