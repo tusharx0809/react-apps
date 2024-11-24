101a91f41de1800168bcc55c94965c67e060af89
tusharx0809 <tusharbhatia0809@outlook.com>
2024-11-24 17:24:54 +0530

    deleted

--- textutils/module1.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Tushar";
-const b = "Vimal";
-const c = "abcde";
-const d = "fghij"
-
-export default b;
-export {a};
-export {c};
-export {d};
--- textutils/module2.mjs
+++
@@ -1,5 +0,0 @@
-import b_vimal, {a, c, d} from './module1.mjs'
-console.log(b_vimal);
-console.log(a);
-console.log(c);
-console.log(d);
