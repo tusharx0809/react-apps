42f0cf0cf23e08ed5dd49ca2dc6fd71c9b56a260
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-11 14:58:39 +0530

    added: added index.js and connection to mongo successfull

---
+++ banking_website/backend/index.js
@@ -0,0 +1,15 @@
+const connecToMongo = require('./db');
+const express = require('express');
+const cors = require('cors');
+
+connecToMongo();
+
+const app = express();
+const port = 5050;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, ()=>{
+    console.log(`Banking Mongo backend is running on https://localhost:${port}`);
+})
