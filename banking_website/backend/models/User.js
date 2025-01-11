ea82772f2ab8435ed5bdf19ea4bb942dc7495b2e
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-11 15:17:07 +0530

    changed: changed user model

--- banking_website/backend/models/User.js
+++ banking_website/backend/models/User.js
@@ -1,26 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    name:{
-        type: String,
-        required: true,
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password:{
-        type: String,
-        required: true,
-    },
-    dob:{
-        type: Date,
-        required: true,
-    },
-    phone:{
-        type: String,
-        required: true,
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isVerified:{
+    type: Boolean,
+    default: false,
+  },
+  otp:{
+    type: String,
+  },
+  otpExpire:{
+    type: Date,
+  }
+
+});
