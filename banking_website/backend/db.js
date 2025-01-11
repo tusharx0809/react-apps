11acc5a756cbdcb7de76a741b352fcef187de9bf
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-11 14:52:27 +0530

    added: db connection file added

---
+++ banking_website/backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongourl = "mongodb://localhost:27017/bankingWebsite";
+
+const connecToMongo = async()=>{
+    try{
+        await mongoose.connect(mongourl);
+        console.log("Connect to Banking Mongo Databse successfully");
+    }catch(error){
+        console.error("Error connecting Banking Mongo Database", mongourl);
+        process.exit(1);
+    }
+}
+
+module.exports = connecToMongo;
