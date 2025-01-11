2ee3efb2f60e1c8fa8136f41279199cc7421c10e
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-11 16:35:43 +0530

    added: added send email utility

---
+++ banking_website/backend/utils/sendEmail.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    const mailOptions = {
+        from: `"Banking Website" <${process.env.EMAIL_USER}>`,
+        to: options.email,
+        subject: options.subject,
+        html: options.message,
+    };
+
+    await transporter.sendEmail(mailOptions);
+};
+
+module.exports = sendEmail;
