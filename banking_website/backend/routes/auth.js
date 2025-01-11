619a5b7bd657bea487449cee8aab597d22a692f8
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-11 16:38:55 +0530

    changes

--- banking_website/backend/routes/auth.js
+++ banking_website/backend/routes/auth.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "8ER3FLQAergfeUIOUiWR3cNWVllBZ2BaFata8pkFnnQYsdCoBH";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 
 
