2f5fe765063bf8ca5b9a18fad0c84f7bd00caa91
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-12 18:33:54 +0530

    added: added provider in profile state

--- banking_website/src/context/Profile/ProfileState.js
+++ banking_website/src/context/Profile/ProfileState.js
@@ -1,37 +1,36 @@
-import React, { useState } from 'react'
-import ProfileContext from './ProfileContext';
+import React, { useState } from "react";
+import ProfileContext from "./ProfileContext";
 
 const ProfileState = (props) => {
-    const host = "http://localhost:5050";
+  const host = "http://localhost:5050";
 
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    const getUserProfile = async () => {
-        const response = await fetch(`${host}/api/auth/getuser`,{
-            method:"GET",
-            headers:{
-                "authToken": localStorage.getItem("token"),
-            },
-        });
-        const json = await response.json();
-        if(json.isVerified){
-            setUser(json);
-        }else{
-            console.log("Error fetching user data");
-        }
+  const getUserProfile = async () => {
+    const response = await fetch(`${host}/api/auth/getuser`, {
+      method: "GET",
+      headers: {
+        authToken: localStorage.getItem("token"),
+      },
+    });
+    const json = await response.json();
+    if (json.isVerified) {
+      setUser(json);
+    } else {
+      console.log("Error fetching user data");
     }
+  };
 
   return (
-    
-      <ProfileContext.Provider
+    <ProfileContext.Provider
       value={{
         getUserProfile,
         user,
-      }}>
-        {props.children}
-      </ProfileContext.Provider>
-    
-  )
-}
+      }}
+    >
+      {props.children}
+    </ProfileContext.Provider>
+  );
+};
 
-export default ProfileState
+export default ProfileState;
