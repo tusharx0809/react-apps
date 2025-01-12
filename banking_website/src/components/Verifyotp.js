1217a76a1036fac3ee33f38c0727d5aa0d1911bd
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-12 14:36:56 +0530

    added: verify otp form added

--- banking_website/src/components/Verifyotp.js
+++ banking_website/src/components/Verifyotp.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Verifyotp = () => {
-  return (
-    <div>
-      <h1>This is verify otp.</h1>
-    </div>
-  )
-}
+    const navigate = useNavigate();
 
-export default Verifyotp
+  return (
+    <div
+      className="container d-flex align-items-center justify-content-center"
+      style={{ minHeight: "90vh" }}
+    >
+      <div className="card p-4" style={{ width: "100%", maxWidth: "400px" }}>
+        <form>
+          <h2 className="text-center mb-4">Verify OTP</h2>
+          <h5 className="mb-4">An OTP has been sent to your email which is valid for next five minutes.</h5>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter OTP..."
+            />
+          </div>
+          
+
+          <div className="d-flex justify-content-around">
+            <button className="btn btn-success mt-2">Verify</button>
+            </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Verifyotp;
