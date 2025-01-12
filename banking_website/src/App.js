3889003429ab26ac254b8c0634acc004580b9874
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-12 13:42:52 +0530

    started: verify otp component started

--- banking_website/src/App.js
+++ banking_website/src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Verifyotp from "./components/Verifyotp";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/verifyotp" element={<Verifyotp />} />
         </Routes>
       </Router>
     </div>
---
+++ banking_website/src/components/Verifyotp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Verifyotp = () => {
+  return (
+    <div>
+      <h1>This is verify otp.</h1>
+    </div>
+  )
+}
+
+export default Verifyotp
