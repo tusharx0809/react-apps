8d7639af1bbbaa09f4da33a8fdc40c3267bc31f4
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-14 18:57:42 +0530

    refactored

--- banking_website/backend/routes/settings.js
+++ banking_website/backend/routes/settings.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.SECRET_JWT;
