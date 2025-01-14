518ab50ae5def72e0ae34e9e17b0afc286c9d988
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-14 18:51:07 +0530

    tested: renew password tested ok on backend, front end to be done

--- banking_website/backend/index.js
+++ banking_website/backend/index.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
-app.use('api/settings', require('./routes/settings'));
+app.use('/api/settings', require('./routes/settings'));
 
 app.listen(port, ()=>{
     console.log(`Banking Mongo backend is running on http://localhost:${port}`);
--- banking_website/backend/routes/settings.js
+++ banking_website/backend/routes/settings.js
@@ -12,35 +12,39 @@ const fetchuser = require("../middleware/fetchuser");
 router.put("/renewpassword", fetchuser, async (req, res) => {
   try {
     let success = false;
-    const userID = req.user.ID;
-
-    const user = User.findById(userID);
-
-    const  curPassword = req.body.curPassword;
-
-    const passwordCompare = await bcrypt(curPassword, user.password);
-    if(!passwordCompare){
-        return res.status(400).json({ success, error: "Current password is incorrect!"});
+    const userID = req.user.id;
+    
+    const user = await User.findById(userID).select("+password");
+    if (!user) {
+      return res.status(404).json({ success, error: "User not found" });
     }
 
-    if(!req.body.newPassword || req.body.newPassword.length < 8){
-        return res.status(400).json({
-            success,
-            error: "Password must be atleast 8 characters long.",
-        })
+    const curPassword = req.body.curPassword;
+
+    const passwordCompare = await bcrypt.compare(curPassword, user.password);
+    if (!passwordCompare) {
+      return res
+        .status(400)
+        .json({ success, error: "Current password is incorrect!" });
+    }
+
+    if (!req.body.password || req.body.password.length < 8) {
+      return res.status(400).json({
+        success,
+        error: "Password must be atleast 8 characters long.",
+      });
     }
 
     const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(req.body.newPassword, salt);
+    user.password = await bcrypt.hash(req.body.password, salt);
 
     await user.save();
     success = true;
 
     res.status(200).json({
-        success,
-        message:"Password has been successfully renewed."
-    })
-
+      success,
+      message: "Password has been successfully renewed.",
+    });
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal server error");
