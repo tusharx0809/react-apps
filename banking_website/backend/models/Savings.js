c564624072029766fa0524de5db084b477d4e44a
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-15 18:40:23 +0530

    added: model for savings account added

---
+++ banking_website/backend/models/Savings.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const ChequingSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId, //connects userobject with notes schema
+        ref: "User",
+        required: true,
+      },
+    amount: {
+        type: Number,
+        default: 0,
+    },
+    interest: {
+        type: Number,
+        default: 3,
+    }
+})
+
+module.exports = mongoose.model('Chequings', ChequingSchema);
