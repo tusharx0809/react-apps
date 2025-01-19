ddc16f19f9fba79e97cba72476f733cbc499171c
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-19 13:19:59 +0530

    added: added wrap in transaction table content

--- banking_website/src/components/Transactions.js
+++ banking_website/src/components/Transactions.js
@@ -6,47 +6,54 @@ const Transactions = () => {
 
   useEffect(() => {
     getTransactions();
-  }, []);
+  }, [getTransactions]);
 
-  const sortedTransactions = transactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+   const sortedTransactions = Array.isArray(transactions)
+    ? transactions.sort((a, b) => new Date(b.date) - new Date(a.date))
+    : [];
 
   return (
     <div>
       <div>
-        <div className="card" style={{height:"351px", overflowY:"scroll"}}>
+        <div className="card" style={{ height: "351px", overflowY: "scroll" }}>
           <div className="card-body">
             <div className="d-flex justify-content-around">
               <div className="col-12">
                 <p className="display-6">Transactions</p>
                 <div className="table-responsive">
-                <table class="table w-100">
-  <thead>
-    <tr>
-      <th scope="col">S.No</th>  
-      <th scope="col">Type</th>
-      <th scope="col">From</th>
-      <th scope="col">To</th>
-      <th scope="col">Amount</th>
-      <th scope="col">Date</th>
-    </tr>
-  </thead>
-  <tbody class="table-group-divider">
-    {sortedTransactions && sortedTransactions.length > 0 ? (
-        sortedTransactions.map((transaction, index)=>(
-            <tr key={index}>
-                <th>{index + 1}</th>
-                <td>{transaction.type}</td>
-                <td>{transaction.from || "Self"}</td>
-                <td>{transaction.to || "Self"}</td>
-                <th>{transaction.amount}</th>
-                <td>{new Date(transaction.date).toLocaleString()}</td>
-            </tr>
-        )) 
-    ):(<tr>
-        <td colSpan="6">No transactions found</td>
-      </tr>)}
-  </tbody>
-</table></div>
+                  <table class="table w-100">
+                    <thead>
+                      <tr>
+                        <th scope="col">S.No</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">From</th>
+                        <th scope="col">To</th>
+                        <th scope="col">Amount</th>
+                        <th scope="col">Date</th>
+                      </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                      {sortedTransactions && sortedTransactions.length > 0 ? (
+                        sortedTransactions.map((transaction, index) => (
+                          <tr key={index}>
+                            <td>{index + 1}</td>
+                            <th>{transaction.type}</th>
+                            <td>{transaction.from || "Checquings"}</td>
+                            <td>{transaction.to || "Checquings"}</td>
+                            <th>{transaction.amount}</th>
+                            <td>
+                              {new Date(transaction.date).toLocaleString()}
+                            </td>
+                          </tr>
+                        ))
+                      ) : (
+                        <tr>
+                          <td colSpan="6">No transactions found</td>
+                        </tr>
+                      )}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
@@ -55,12 +62,17 @@ const Transactions = () => {
       <style>
         {`
           .table-responsive {
-            overflow-x: auto; /* Keep responsiveness */
+            overflow-x: auto; 
           }
-          .table td,  {
+          .table td {
             word-wrap: break-word;
             word-break: break-word;
-            white-space: normal; /* Enable text wrapping */
+            white-space: normal; 
+            max-width: 2000px;  
+          }
+          
+          .table th {
+            white-space: nowrap;
           }
         `}
       </style>
